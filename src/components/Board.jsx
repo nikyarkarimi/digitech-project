f0baaed40faf4c0199b543a45b4d4af388301398
TicTacTam <146174713+TicTacTam@users.noreply.github.com>
2025-06-08 14:59:26 +0200

    Added connection visualization

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -19,15 +19,40 @@ export default function Board() {
       } else {
         console.log("New connection set!");
         connectionsRef.current.push([selectedNodeRef.current, id]);
+        drawLineBetween(selectedNodeRef.current, id);
         selectedNodeRef.current = null;
-      console.log("All connections:", connectionsRef.current);
+        console.log("All connections:", connectionsRef.current);
       }
     };
+
+    const drawLineBetween = (fromId, toId) => {
+      const svg = containerRef.current.querySelector("svg");
+      const from = svg.querySelector(`#${fromId}`);
+      const to = svg.querySelector(`#${toId}`);
+
+      if (!from || !to) return;
+
+      const x1 = parseFloat(from.getAttribute("cx"));
+      const y1 = parseFloat(from.getAttribute("cy"));
+      const x2 = parseFloat(to.getAttribute("cx"));
+      const y2 = parseFloat(to.getAttribute("cy"));
+
+      const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      line.setAttribute("x1", x1);
+      line.setAttribute("y1", y1);
+      line.setAttribute("x2", x2);
+      line.setAttribute("y2", y2);
+      line.setAttribute("class", "cable-red");
+
+      // put lines at the top level of the SVG for visibility
+      svg.appendChild(line);
+    };
+
 
     fetch("src/assets/Digitech.svg")
       .then((res) => res.text())
       .then((svg) => {
-        console.log("Baby we're");
+        console.log("Baby we're onnn");
         // this is the response from the previous block, svg being a variable name
         if (!containerRef.current) return;
         // injects svg as text into the HTML, allowing us to use functions like the query selector - now we can access the paths
@@ -38,6 +63,7 @@ export default function Board() {
           const circle = event.target.closest("circle[id]");
           if (circle) {
             handleNodeClick(circle.id);
+            
           }
         };
         svgElement.addEventListener("click", handleClick);
